4dcc533dafe820958cd4ab9986b79d06dc89c1fd
CountNeuroman <olorinoren@gmail.com>
2024-12-05 23:37:14 +0300

    Init solution

---
+++ RedNosedReports/Program.cs
@@ -0,0 +1,17 @@
+﻿var inputString = File.ReadAllText("input.txt").Split("\n");
+
+var data = new List<List<int>>();
+foreach (var s in inputString)
+{
+    var tmp = s.Split(" ");
+    var tmpList = new List<int>();
+    foreach (var s1 in tmp)
+    {
+        tmpList.Add(Convert.ToInt32(s1));
+    }
+    data.Add(tmpList);
+}
+
+
+
+Console.ReadLine();
