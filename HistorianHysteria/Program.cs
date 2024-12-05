45694edabfae5f1dc0c10b3d49b454385cd84d0b
CountNeuroman <olorinoren@gmail.com>
2024-12-05 22:34:46 +0300

    Add part one solution

--- HistorianHysteria/Program.cs
+++ HistorianHysteria/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var input = File.ReadAllText("input.txt").Split("\n");
 
-Console.WriteLine("Hello, World!");
+var leftNumbers = new List<int>();
+var rightNumbers = new List<int>();
+foreach (var str in input)
+{ 
+    var tmp = str.Split("   ");
+    leftNumbers.Add(Convert.ToInt32(tmp[0]));
+    rightNumbers.Add(Convert.ToInt32(tmp[1]));
+}
+
+leftNumbers.Sort();
+rightNumbers.Sort();
+
+var result = 0;
+for (var i = 0; i < leftNumbers.Count; i++)
+{
+    result += Math.Abs(leftNumbers[i] - rightNumbers[i]);  
+}
+
+Console.WriteLine($"Result: {result}");
